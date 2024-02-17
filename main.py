35df996e8e7bf3d5bc720b3c83eb98698112bef4
J0hn@th@N <13274806+johnathanavi@users.noreply.github.com>
2024-02-17 15:29:14 -0500

    Update main.py
    Start password generator functions, separate numbers, random letters and random term for password

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+# Password generator function
+def random_number():
+    number1 = str(random.randint(0,9))
+    number2 = str(random.randint(0,9))
+    number3 = str(random.randint(0,9))
+    number4= str(random.randint(0,9))
+    final_number = "".join(number1,number2,number3,number4)
+    return final_number
+
+def random_strings():
+    letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    final_letters = random.choice(letters) + random.choice(letters) + random.choice(letters) + random.choice(letters) + random.choice(letters)
+    return final_letters
+
+def random_term():
+    terms = [
+        
+    ]
+
+def generate_password():
+    pass
+
