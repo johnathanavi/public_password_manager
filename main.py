418c2032e8ec09ae7fe126733418a88b397ccc6c
J0hn@th@N <13274806+johnathanavi@users.noreply.github.com>
2024-02-17 15:35:26 -0500

    Update main.py
    Test random custom functions

--- main.py
+++ main.py
@@ -13,19 +13,19 @@ def random_number():
     number2 = str(random.randint(0,9))
     number3 = str(random.randint(0,9))
     number4= str(random.randint(0,9))
-    final_number = "".join(number1,number2,number3,number4)
+    numbers = [number1, number2, number3, number4]
+    final_number = "".join(numbers)
     return final_number
 
 def random_strings():
     letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    final_letters = random.choice(letters) + random.choice(letters) + random.choice(letters) + random.choice(letters) + random.choice(letters)
+    final_letters = random.choice(letters) + random.choice(letters) + random.choice(letters) + random.choice(letters)+ random.choice(letters)
     return final_letters
 
 def random_term():
     terms = [
-        
+
     ]
 
 def generate_password():
     pass
-
